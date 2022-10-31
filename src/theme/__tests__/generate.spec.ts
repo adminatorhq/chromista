dcecbc08841f157d73b696f1e07af0a6d52748a3
Ayobami Akingade <akingbadefred@gmail.com>
2022-10-31 23:27:33 +0100

    test: add tests for generating

--- src/theme/__tests__/generate.spec.ts
+++ src/theme/__tests__/generate.spec.ts
@@ -1,1 +1,22 @@
-describe("generateRootColors", () => {});
+import { DEFAULT_PRIMARY_COLOR } from "../constants";
+import { generateRootColors } from "../generate";
+import { LIGHT_MODE } from "../modes";
+
+describe("generateRootColors", () => {
+  it("should generate correct root colors", () => {
+    expect(generateRootColors(DEFAULT_PRIMARY_COLOR, LIGHT_MODE))
+      .toMatchInlineSnapshot(`
+      Object {
+        "--hadmean-base-color": "#ffffff",
+        "--hadmean-border-color": "#e3ebf6",
+        "--hadmean-foundation-color": "#f3f6f9",
+        "--hadmean-main-text": "#5f6270",
+        "--hadmean-muted-text": "#a4abc5",
+        "--hadmean-primary-color": "#4b38b3",
+        "--hadmean-primary-shade-color": "#4b38b31A",
+        "--hadmean-soft-color": "#f1f5fa",
+        "--hadmean-text-on-primary": "#ffffff",
+      }
+    `);
+  });
+});
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,4 @@
 export { IColorMode } from "./types";
 export { USE_ROOT_COLOR } from "./root";
 export { SYSTEM_COLORS } from "./system";
+export { useTheme } from "./useTheme";
